5c04619a066c271c2fcea7bf64eab6740dc8b660
Ramkrishna Kulkarni <ramkrishna.kulkarni@gmail.com>
2021-05-31 16:06:00 +0530

    required bridge config keys to be set

--- PHP_BridgePG/bridge_config.php
+++ PHP_BridgePG/bridge_config.php
@@ -3,4 +3,12 @@ defined('MERCHANT_ID')   OR define('MERCHANT_ID' , '', TRUE);
 defined('PUBLIC_KEY')    OR define('PUBLIC_KEY', '', TRUE);
 defined('PRIVATE_KEY')   OR define('PRIVATE_KEY' , '', TRUE);
 
+defined('RETURN_URL')  OR define('RETURN_URL' , '', TRUE);
+defined('CANCEL_URL')  OR define('CANCEL_URL' , '', TRUE);
+defined('PRODUCT_ID')  OR define('PRODUCT_ID' , '', TRUE);
+
+
+defined('API_KEY') OR define('API_KEY', "", TRUE);
+defined('API_URL') OR define('API_URL', "", TRUE);
+
 //End of bridge_config ..
